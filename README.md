874c256a0186800f6cd3a08c0eb2cf19ee15cdc4
Emmanuel Nunez <emmanuel@Emmanuels-MacBook-Air.local>
2022-03-31 20:48:13 -0400

    db/connection file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Top-100-Superhero-Movies-API
+# IMBD's-Top-100-Superhero-Movies-API
+
 A JSON API using Mongoose and Express on IMDB's Top 100 Superhero movies (as of 03/30/22)
---
+++ db/connection.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI =
+  process.env.PROD_MONGODB ||
+  "mongodb:/127.0.0.1:27017/IMDB's-Top-100-Superhero-Movies";
+
+mongoose.set("returnOriginal", false);
+
+mongoose
+  .connect(MONGODB_URI)
+  .catch((error) =>
+    console.error("Error connecting to MongoDB: ", error.message)
+  );
+
+mongoose.connection.on("disconnected", () =>
+  console.log("Disconnected from MongoDB!")
+);
+
+mongoose.connection.on("error", (error) =>
+  console.error(`MongoDB connection error: ${error}`)
+);
+
+export default mongoose.connection;
