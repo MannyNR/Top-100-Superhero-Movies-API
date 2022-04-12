7fa29996ec5a25305d13df339da7dce203c0df5f
Emmanuel Nunez <emmanuel@Emmanuels-MacBook-Air.local>
2022-04-12 13:57:26 -0400

    cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import db from "./db/connection.js";
 import routes from "./routes/index.js";
-
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", routes);
 
 db.on("connected", () => {
